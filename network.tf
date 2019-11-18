7c6e866ad1e42ad6c71b74e84dffef0274dee62e
logicboy <faithfulanere@gmail.com>
2019-11-19 01:21:06 +0800

    adjust availability zones

--- network.tf
+++ network.tf
@@ -1,7 +1,7 @@
 #provision vpc, igw, subnets and default route-table
 #1 VPC - 3 subnets (public, web , database)
 
-#provision app vpc
+# provision app vpc
 resource "aws_vpc" "app_vpc" {
   cidr_block           = "${var.vpc_cidr}"
   instance_tenancy     = "default"
@@ -22,18 +22,20 @@ resource "aws_internet_gateway" "app_gw" {
 # provision public subnet using length function to create 2 subnets at once
 # use element function to pick one element at a time for the subnet_cidr
 resource "aws_subnet" "public_subnets" {
-  count      = "${length(var.azs)}"
-  vpc_id     = "${aws_vpc.app_vpc.id}"
-  cidr_block = "${element(var.public_subnet_cidr, count.index)}"
+  count             = "${length(var.azs)}"
+  vpc_id            = "${aws_vpc.app_vpc.id}"
+  cidr_block        = "${element(var.public_subnet_cidr, count.index)}"
+  availability_zone = "${element(var.azs, count.index)}"
   tags = {
     Name = "genesis-public-subnet-${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "private_subnets" {
-  count      = "${length(var.azs)}"
-  vpc_id     = "${aws_vpc.app_vpc.id}"
-  cidr_block = "${element(var.private_subnet_cidr, count.index)}"
+  count             = "${length(var.azs)}"
+  vpc_id            = "${aws_vpc.app_vpc.id}"
+  cidr_block        = "${element(var.private_subnet_cidr, count.index)}"
+  availability_zone = "${element(var.azs, count.index)}"
   tags = {
     Name = "genesis-private-subnet-${count.index + 1}"
   }
