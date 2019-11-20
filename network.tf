b1d8c01efd8c0c83f517158e846e1eec981ce9fd
logicboy <faithfulanere@gmail.com>
2019-11-20 17:33:06 +0800

    Network - added security group for inbound and outbound rules

--- network.tf
+++ network.tf
@@ -1,11 +1,11 @@
 #provision vpc, igw, subnets and default route-table
-#1 VPC - 3 subnets (public, web , database)
+#1 VPC - 4 subnets (application tier, web tier, data tier)
 
-# vpc
 resource "aws_vpc" "app_vpc" {
   cidr_block           = var.vpc_cidr
   instance_tenancy     = "default"
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Name = "genesis-app-vpc"
@@ -78,22 +78,23 @@ resource "aws_route_table_association" "app_rt_public_as" {
   count          = length(var.public_subnet_cidr)
   subnet_id      = element(aws_subnet.app_public_subnets.*.id, count.index)
   route_table_id = aws_route_table.app_public_rt.id
+  depends_on     = [aws_route_table.app_public_rt, aws_subnet.app_public_subnets]
 }
-
 
 # Route table association: attach with private subnets
 resource "aws_route_table_association" "app_rt_private_as" {
   count          = length(var.private_subnet_cidr)
   subnet_id      = element(aws_subnet.app_private_subnets.*.id, count.index)
   route_table_id = aws_route_table.app_private_rt.id
+  depends_on     = [aws_route_table.app_private_rt, aws_subnet.app_private_subnets]
 }
 
 # Elastic ip: for nat gateway
 resource "aws_eip" "app_eip" {
-  vpc      = true
+  vpc = true
 }
 
-# NAT Gateway: seats on 1 public subnet and exposes private subnets to the internet
+# NAT Gateway: seats on 1 public subnet and exposes private subnets to the net
 resource "aws_nat_gateway" "app_nat_gw" {
   allocation_id = aws_eip.app_eip.id
   subnet_id     = aws_subnet.app_public_subnets[0].id
@@ -104,4 +105,38 @@ resource "aws_nat_gateway" "app_nat_gw" {
   depends_on = [aws_internet_gateway.app_gw]
 }
 
+# Security Group Creation
+resource "aws_security_group" "app_security_group" {
+  name   = "genesis-sg"
+  vpc_id = aws_vpc.app_vpc.id
+}
+
+# Ingress Security Port 22
+resource "aws_security_group_rule" "ssh_inbound_access" {
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.app_security_group.id
+  to_port           = 22
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "http_inbound_access" {
+  from_port         = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.app_security_group.id
+  to_port           = 80
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# All OutBound Access
+resource "aws_security_group_rule" "all_outbound_access" {
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.app_security_group.id
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
